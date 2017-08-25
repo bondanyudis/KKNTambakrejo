716d9a7e21e5672442a5d987bbd6a5410b40b718
yudistira <yudistira96@gmail.com>
2017-08-25 07:10:10 +0700

    tampilan awal

---
+++ js/style.js
@@ -0,0 +1,40 @@
+// dropdown
+$(document).ready(function()
+{
+  $('#load-more-content').click(function()
+  {
+    
+    $('#more-content').toggleClass('shown');
+
+    $('#load-more-content').hide();
+
+    if( $('#more-content').hasClass('shown') )
+    {
+      $('#load-more-content').text('Hide content');
+      $('#more-content').fadeIn('slow', function()
+      {
+        $('#load-more-content').fadeIn('slow');
+      });
+    }
+    else
+    {
+      $('#load-more-content').text('Load some content');
+      $('#more-content').fadeOut('slow', function()
+      {
+        $('#load-more-content').fadeIn('slow');
+      });
+    }
+  });
+ 
+});
+// end dropdown
+// hide top menu
+ function scrollFunction() {
+if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+document.getElementById('inner').style.marginTop="-50px";
+} else {
+document.getElementById('inner').style.marginTop="0px";
+}
+}
+window.onscroll = function() {scrollFunction()};
+// 
