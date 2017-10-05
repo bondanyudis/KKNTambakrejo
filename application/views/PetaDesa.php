d36299a60745cce3dcf5b7f362856dbc9dd89a08
Fajar2009 <noorfajar96@gmail.com>
2017-10-05 16:07:11 +0700

    Tambahan page peta desa
    peta desa menggunakan google map

---
+++ application/views/PetaDesa.php
@@ -0,0 +1,25 @@
+<!-- Tutorial URL: http://sixrevisions.com/css/responsive-background-image/ -->
+<!doctype html>
+<html lang="en">
+  <head>
+   <?php include('partials/Head.php') ?>
+  </head>
+  <body>
+    <?php include('partials/Header.php') ?>
+    <h1 >Peta Desa Tambakrejo</h1>
+    <div id="googleMap" style="width:100%;height:800px;" align="center"></div>
+
+<script>
+function myMap() {
+var mapProp= {
+    center:new google.maps.LatLng(-8.4109855,112.6971755),
+    zoom:15,};
+var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
+</script>
+
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBZG6z9_tXeHTn7BQ6WU2BECcNrV5hcrys&callback=myMap"></script>
+<span class="clearfix"></span>
+<?php include('partials/Footer.php') ?>
+</body>
+</html>
